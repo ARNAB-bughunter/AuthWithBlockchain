d2e46928bbb8d41cc98e7caa1cca58029e4dd5a5
ARNAB-bughunter <arnabnaha219@gmail.com>
2022-03-24 16:30:02 +0530

    Compress Image

--- Base/models.py
+++ Base/models.py
@@ -9,17 +9,13 @@ class productDetail(models.Model):
     image = models.ImageField(upload_to = 'product')
 
     created=models.DateTimeField(auto_now=True)
-    def resize(self,im,new_height):
-        width,height = im.size
-        ratio = height/width
-        new_width = int(ratio*new_height)
-        resized_image = im.resize((new_width,new_height))
-        return resized_image
+    
     
     def save(self,*args,**kwargs):
         super().save(*args,**kwargs)
         img=Image.open(self.image.path)
-        img_resized = self.resize(img,250)
+        height, width = img.size
+        img_resized =img.resize((height,width), Image.ANTIALIAS)
         img_resized.save(self.image.path)
 
          
